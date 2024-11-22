1a86cb68121de448443380c1c6f19d8db8e81964
Andrew Ross <andrew.ross@takeda.com>
2024-11-22 03:03:37 -0600

    mucho

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,10 @@ import baseConfig from "@fade/eslint-config/base";
 export default [
   {
     rules: {
-      "@typescript-eslint/consistent-type-definitions": "off"
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-assertions": "off",
+      "@typescript-eslint/require-await": "off",
+      "import/consistent-type-specifier-style": "off"
     },
     ignores: ["**node_modules**"]
   },
