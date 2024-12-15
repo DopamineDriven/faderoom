1b33987859186430c1bf8e15574582f57bfd28a7
Andrew Ross <andrew.ross@takeda.com>
2024-12-15 03:54:17 -0600

    updates

--- apps/web/src/ui/nav/index.tsx
+++ apps/web/src/ui/nav/index.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { motion } from "framer-motion";
-import { Scissors } from "lucide-react";
 import { FadeRoomIcon } from "../icons/FadeRoom";
 
 const NavBar = () => {
